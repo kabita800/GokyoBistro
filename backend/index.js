a097e00716e99ecd7fa2275d82998d70245d72ee
8972-amick <khadkaamick2@gmail.com>
2026-04-09 09:25:34 +0545

    feat: Server for Gokyo setup successfully

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Gokyo Bistro!');
+});
+
+app.listen(3000, () => {
+    console.log("Gokyo Bistro backend is running on port 3000");
+})
