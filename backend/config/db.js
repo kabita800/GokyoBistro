146c9bded927781a4647d72ce3b966ae28af9e0a
8972-amick <khadkaamick2@gmail.com>
2026-04-12 21:17:00 +0545

    initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 
-
+import mongoose from "mongoose";
 const connectDB = async () => {
   try {
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -13,4 +14,3 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
